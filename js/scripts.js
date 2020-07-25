2e4a32855e93e9db809e8ba96a0375d5539285e8
George Wayne Mwangi <waynegeorge193.com>
2020-07-25 18:24:36 +0300

    add a jquery function to show pop once form is submiited.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $("form#contacts").submit(function(event) {
+
+    var names= $("#name").val();
+    var email= $("#email").val();
+    var message = $("#YourMessage").val();
+    alert("Dear " +names+ ",email address "+ email+ ",your message has been received. Thank you for contacting us");
+    event.preventDefault();
+
   $(".clickable1").click(function() {
     $(".design-show").toggle();
     $(".design-hide").toggle();
@@ -11,4 +19,5 @@ $(document).ready(function() {
     $(".productManagement-show").toggle();
     $(".productManagement-hide").toggle();
   });
+});
 });
