b52e59e092d92b0cc4aab651b57a19a1f528c86b
George Wayne Mwangi <waynegeorge193.com>
2020-07-25 14:44:30 +0300

    use jQuery to make the What We Do section icons when clicked toggle between the respective description of each icon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $(".clickable1").click(function() {
     $(".design-show").toggle();
     $(".design-hide").toggle();
+  });
+  $(".clickable2").click(function() {
+    $(".development-show").toggle();
+    $(".development-hide").toggle();
+  });
+  $(".clickable3").click(function() {
+    $(".productManagement-show").toggle();
+    $(".productManagement-hide").toggle();
   });
 });
